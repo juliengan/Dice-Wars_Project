83207e382a920241496944390ecfadb5473547a0
Nguyen <florence.nguyen@efrei.net>
2020-12-15 10:25:51 +0100

    resolve

--- src/Exercise1/Main.java
+++ src/Exercise1/Main.java
@@ -3,7 +3,11 @@ package Exercise1;
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< Updated upstream
 	    System.out.println("Hello");
         System.out.println("s/o Thao-ly le sang du bibi she saved me !!!!");
+=======
+	// 
+>>>>>>> Stashed changes
     }
 }
