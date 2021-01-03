271c932385377c68b82435d1d4c8e9a5472bbb47
Nguyen <florence.nguyen@efrei.net>
2021-01-03 11:55:24 +0100

    attackTerritory sécurisé

--- src/Datas/Player.java
+++ src/Datas/Player.java
@@ -23,31 +23,64 @@ public class Player {
     }
     public void attackTerritory(Scanner input)
     {
-        Move move;
+        Move move = null;
         int territoryIdAttack;
         int territoryIdDefend;
         Integer d1, d2;
         Integer attack;
         Integer defend;
 
-        System.out.print("Enter your territory : ");
-        attack = input.nextInt();
-        System.out.print("Enter the territory you want to attack : ");
-        defend = input.nextInt();
+        while(move == null) {
+
+            System.out.print("Enter your territory : ");
+            attack = input.nextInt();
+
+            if (!this.territories.contains(getTerritoryById(attack))) {
+                System.out.println("This is not your territory, you can't attack");
+                continue;
+            }
+
+
+            System.out.print("Enter the territory you want to attack : ");
+            defend = input.nextInt();
+
+
+          if(getTerritoryById(attack).getNeighboringTer().contains(getTerritoryById(defend))){
+              System.out.println("The territory you want to attack is not your neighbour ");
+              continue;
+          }
+          else if(this.territories.contains(getTerritoryById(defend))){
+              System.out.println("You can't attack your own territory ! ");
+              continue;
+          }
 
 
 
 
-        move = new Move(attack, defend);
-
-        System.out.println("Your move is : ("+ move.getIdAttacker()+ " "+move.getIdDefender()+")");
+            move = new Move(attack, defend);
 
 
 
+        }
 
+        System.out.println("Your move is : (" + move.getIdAttacker() + " " + move.getIdDefender() + ")");
 
 
     }
+
+    //This function return a territory for a given ID
+    public Territory getTerritoryById(int id){
+
+        for(Territory t : this.territories){
+            if(t.getId() == id){
+                return  t;
+            }
+        }
+        return null;
+    }
+
+
+
     public void endTurn()
     {
 
